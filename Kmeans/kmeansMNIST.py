621349d5eef0d3535c5b962ff4e89dacc6294b8a
Krish Ganotra <krishganotra@gmail.com>
2020-12-16 01:51:33 -0500

    Updated Kmeans MNIST

--- Kmeans/kmeansMNIST.py
+++ Kmeans/kmeansMNIST.py
@@ -1,21 +1,29 @@
 import sklearn
-from sklearn.datasets import load_digits
+import sklearn.datasets as datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.cluster import KMeans
-from keras.datasets import mnist
+from sklearn.cluster import MiniBatchKMeans
+import keras
+import time
 
 np.random.seed(42)
 
-# X_digits, y_digits = load_digits(return_X_y=True)
+# X_digits, y_digits = datasets.load_digits(return_X_y=True)
+# X_digits, y_digits = datasets.fetch_openml('mnist_784', return_X_y=True)
 # X_train, X_test, y_train, y_test = train_test_split(X_digits, y_digits, random_state=42)
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 X_train = X_train.reshape(X_train.shape[0], -1)
 X_test = X_test.reshape(X_test.shape[0], -1)
 
-k = 50
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-kmeans = KMeans(n_clusters=k, random_state=42, n_init=1).fit(X_train)
+k = 100
+start = time.time()
+
+kmeans = MiniBatchKMeans(n_clusters=k, random_state=42, n_init=1).fit(X_train)
+# kmeans = KMeans(n_clusters=k, random_state=42, n_init=1).fit(X_train)
 y_pred_indices = kmeans.predict(X_test)
 
 X_digits_dist = kmeans.transform(X_train)
@@ -25,3 +33,4 @@ y_pred = y_representative_digits[y_pred_indices]
 
 accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy} with k={k}")
+print(f"Time taken: {time.time()-start}")
